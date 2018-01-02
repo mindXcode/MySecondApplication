d286595b72bf2fae9c0bfa57625999774a0dd1c0
mindXcode <33657397+mindXcode@users.noreply.github.com>
2018-01-02 13:19:48 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/mabuzaid/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mabuzaid/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.print("HelloWorld");
+        System.out.print("line added by another developer");
     }
 }
 
